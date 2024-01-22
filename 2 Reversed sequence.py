7b37e5d7575cb1386bf1902003d53c70f353afde
SyahrulApr86 <apriansyah.syahrul@gmail.com>
2024-01-22 22:08:28 +0700

    chal 2

---
+++ 2 Reversed sequence.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/5a00e05cc374cb34d100000d/solutions/python
+
+def reverse_seq(n):
+    lst = []
+
+    while n > 0:
+        lst.append(n)
+        n -= 1
+
+    return lst
+
+
+# def reverse_seq(n):
+#     return range(n, 0, -1)
+
