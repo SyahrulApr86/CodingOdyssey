a378a8deaa6c0eb87773422a737e0c9cc24d971e
SyahrulApr86 <94279620+SyahrulApr86@users.noreply.github.com>
2024-01-29 21:25:44 +0700

    #26

---
+++ 26 Rot13/main.py
@@ -0,0 +1,25 @@
+# https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+
+def rot13(message):
+    res = ''
+    for i in message:
+        if 'A' <= i <= 'Z':
+            ascii_i = ord(i) + 13
+            if ascii_i > ord('Z'):
+                ascii_i -= 26
+        elif 'a' <= i <= 'z':
+            ascii_i = ord(i) + 13
+            if ascii_i > ord('z'):
+                ascii_i -= 26
+        else:
+            res += i
+            continue
+        res += chr(ascii_i)
+    return res
+
+
+
+# print(rot13('aA bB zZ 1234 *!?%'))
+
+
+print(rot13('z'))
